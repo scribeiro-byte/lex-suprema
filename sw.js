92ebee86293be3e50f2fcb45d777e6c34d53b425
scribeiro-byte <scr.pessoal01@gmail.com>
2026-03-12 20:51:31 -0300

    Lex Mitio v2 — sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lex-mitio-v1';
+const CACHE='lex-mitio-v2';
 const FILES=['./','/lex-suprema/','/lex-suprema/index.html'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
