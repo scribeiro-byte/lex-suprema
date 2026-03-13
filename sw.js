b001e24fcef2968d7d4cc164aeef41691eda7497
scribeiro-byte <scr.pessoal01@gmail.com>
2026-03-13 07:27:09 -0300

    chore: sw.js v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lm-v6';
+const CACHE='lm-v7';
 const ASSETS=['./','./?pwa=1','./manifest.json'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
