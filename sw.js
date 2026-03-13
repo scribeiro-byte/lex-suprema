ba27c9bc9a4bbb154688d1ac790dbda64684d068
scribeiro-byte <scr.pessoal01@gmail.com>
2026-03-12 22:01:49 -0300

    feat: SW v4 cache limpo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lex-mitio-v3';
+const CACHE='lex-mitio-v4';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
